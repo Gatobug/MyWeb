d69382ca50045f2dfea8baf9cb4a6fddcd8e9e26
matcar2007 <jaime.velasquez@puentemaipo.cl>
2026-09-10 09:25:41 -0300

    MyWeb

--- app.js
+++ app.js
@@ -35,6 +35,11 @@ console.log(user);
 // Configurar EJS
 app.set('view engine', 'ejs');
 
+// MIDDLEWARES
+// Permite leer informacion enviada por formulario
+// html
+app.use(express.urlencoded({ extended: true}));
+
 // Archico Estaticos
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -53,6 +58,21 @@ app.get('/nosotros', (req,res) => {
 
 app.get('/contacto', (req,res) => {
   res.render('contacto');
+});
+
+app.post('/contacto', (req, res) => {
+
+  const { nombre, correo, asunto, mensaje } = req.body;
+
+  console.log('-------------');
+  console.log('Mensaje Falaziento');
+  console.log('-------------');
+  console.log('Nombre:', nombre);
+  console.log('Correo:', correo);
+  console.log('Asunto:', asunto);
+  console.log('Mensaje:', mensaje);
+
+  rep.render('index');
 });
 
 
